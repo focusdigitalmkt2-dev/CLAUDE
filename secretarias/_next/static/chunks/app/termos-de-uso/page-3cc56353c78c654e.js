1d53063988f2c10038743def9c318b85307b53b5
focusdigitalmkt2-dev <focusdigitalmkt2-dev@users.noreply.github.com>
2026-09-23 19:25:04 +0000

    deploy: landing page secretárias (558f993552822b95d78693d844c0c92e9b6d2880) 558f993552822b95d78693d844c0c92e9b6d2880

---
+++ secretarias/_next/static/chunks/app/termos-de-uso/page-3cc56353c78c654e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[55,81],{2573:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,2619,23)),Promise.resolve().then(n.t.bind(n,1356,23)),Promise.resolve().then(n.bind(n,6957))}},e=>{e.O(0,[297,72,957,441,255,358],()=>e(e.s=2573)),_N_E=e.O()}]);
